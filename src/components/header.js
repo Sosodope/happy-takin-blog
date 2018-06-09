0b22f7053441e1da29aba8afff82a9e101d594a3
Sosodope <sonadolkar93@hotmail.com>
2018-06-10 03:47:21 +0700

    Add several components and pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,95 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
-const blue = '#5BC0EB'
-const yellow = '#FDE74C'
+const blue = "#5BC0EB";
+const yellow = "#FDE74C";
 const Header = ({ siteTitle, siteLogo }) => (
   <div
     style={{
       background: blue,
-      marginBottom: '1.45rem',
+      marginBottom: "1.45rem"
     }}
   >
     <header
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(10, 1fr)',
+        padding: "1.45rem 1.0875rem",
+        display: "grid",
+        gridTemplateColumns: "repeat(10, 1fr)"
       }}
     >
-      <h1 style={{ margin: 0, gridColumn: 'span 3' }}>
+      <img
+        src={siteLogo}
+        style={{
+          borderRadius: "5%",
+          width: "100%",
+          paddingRight: "30px",
+          alignSelf: "center"
+        }}
+      />
+      <h1 style={{ margin: 0, gridColumn: "2 / span 5" }}>
         <Link
           to="/"
           style={{
             color: yellow,
-            textDecoration: 'none',
-            textShadow: '1px 1px 6px black',
+            textDecoration: "none",
+            textShadow: "1px 1px 6px black"
           }}
+          className="links"
         >
           {siteTitle}
         </Link>
       </h1>
       <nav
         style={{
-          gridColumn: '10',
+          gridColumn: "10"
         }}
       >
-        <img src={siteLogo} style={{ borderRadius: '5%' }} />
+        <ul style={{ listStyle: "none", float: "right", display: "flex" }}>
+          <li style={{ display: "inline-block", marginRight: "1rem" }}>
+            <Link
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontSize: "x-large"
+              }}
+              to="/"
+              className="links"
+            >
+              Home
+            </Link>
+          </li>
+          <li style={{ display: "inline-block", marginRight: "1rem" }}>
+            <Link
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontSize: "x-large"
+              }}
+              to="/about"
+              className="links"
+            >
+              About
+            </Link>
+          </li>
+          <li style={{ display: "inline-block", marginRight: "1rem" }}>
+            <Link
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontSize: "x-large"
+              }}
+              to="/page-3"
+              className="links"
+            >
+              More
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   </div>
-)
+);
 
-export default Header
+export default Header;
---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const ThridPage = () => (
+  <div>
+    <h1>Third Page</h1>
+    <p>This is my first Gtasby site</p>
+  </div>
+);
+
+export default ThridPage;
