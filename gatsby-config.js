e360e99e397d6736d6d4e3e862858892db6f0854
Sosodope <sonadolkar93@hotmail.com>
2018-06-10 15:07:29 +0700

    Setup environment variables with dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
-    title: 'Happy Takin',
+    title: "Happy Takin"
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: ["gatsby-plugin-react-helmet"]
+};
